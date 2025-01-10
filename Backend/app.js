ab4bc54c05c96298143064da710472a51b6eb00d
alokkumar07 <aloknith07@gmail.com>
2025-01-10 07:33:43 +0530

    mongoose setup

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,9 @@ dotenv.config()
 const express = require('express');
 const app = express();
 
+const connectToDb = require('./db/db')
+connectToDb()
+
 const cors = require('cors')
 app.use(cors());
 app.get('/', (req, res) =>{
